17a1edfca09712ee8eab2d8ffd68fe9c2a1ee4f8
Denis Haumont <76622294+dhaumont@users.noreply.github.com>
2026-03-01 19:04:17 +0100

    fix generateProject

--- generateProject.sh
+++ generateProject.sh
@@ -1,12 +1,13 @@
 #! /bin/bash
 
-mkdir debug
+export LEDA_DIR=$HOME/code/LEDA-7
+mkdir -p debug
 cd debug
 cmake -DCMAKE_BUILD_TYPE=Debug ..
 make
 cd ..
 
-mkdir release
+mkdir -p release
 cd release
 cmake -DCMAKE_BUILD_TYPE=Release ..
 make
