2741dceb611fb73aaafbba088ab8a92392add03e
Denis Haumont <76622294+dhaumont@users.noreply.github.com>
2026-03-23 23:17:34 +0100

    callstack

--- demo/demo_main.cpp
+++ demo/demo_main.cpp
@@ -36,7 +36,11 @@ along with Visilib. If not, see <http://www.gnu.org/licenses/>
 #include <fstream>
 #define _USE_MATH_DEFINES
 #include <math.h>
-
+#include <stdio.h>
+#include <execinfo.h>
+#include <signal.h>
+#include <stdlib.h>
+#include <unistd.h>
 // #include "xmmintrin.h"
 // #include "pmmintrin.h"
 
@@ -388,8 +392,22 @@ void animate()
     demo->animate();
 }
 
+void handler(int sig) {
+  void *array[20];
+  size_t size;
+
+  // get void*'s for all entries on the stack
+  size = backtrace(array, 20);
+
+  // print out all the frames to stderr
+  fprintf(stderr, "Error: signal %d:\n", sig);
+  backtrace_symbols_fd(array, size, STDERR_FILENO);
+  exit(1);
+}
 int main(int argc, char** argv)
 {
+    signal(SIGSEGV, handler);  
+    signal(SIGABRT, handler);  
     demo = new VisilibDemoMain();
 
     demo->writeHelp();
